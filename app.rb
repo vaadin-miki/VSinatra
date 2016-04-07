59ebba4191e39fcedde9a42a553b015d90456d6b
Miki <miki@vaadin.com>
2016-04-07 13:22:32 +0300

    date picker uses its own helper in the application

--- app.rb
+++ app.rb
@@ -26,7 +26,8 @@ class VElementsApp < Sinatra::Base
     params[:value].nil? || params[:value].empty? ? @selection.delete(params[:id]) : @selection[params[:id]] = params[:value]
     puts "SELECT #{@selection.inspect}"
 
-    people = Person.find_all.select { |p| (@selection["birthplace"].nil? || p.country == @selection["birthplace"]) && (@elements.date.value.nil? || @elements.date.value.empty? || p.year.to_s == @elements.date.value[0...4])}
+    people = Person.find_all.select { |p| (@selection["birthplace"].nil? || p.country == @selection["birthplace"]) && (@selection["date"].nil? || p.year.to_s == @selection["date"][0...4]) }
+
     content_type "application/json", :charset => 'utf-8'
     with_this({grid: {items: people}}.to_json) {|x| puts "ANSWER #{x.inspect}"}
   end
@@ -34,7 +35,6 @@ class VElementsApp < Sinatra::Base
   post '/~/:id' do
     case params["id"]
       when "person" then
-        puts "-> PRS #{params['value']}"
         content_type("application/json")
         {birthplace: {value: (Person.find_by_id(params["value"]).country rescue "")}}.to_json
       else
@@ -47,9 +47,6 @@ class VElementsApp < Sinatra::Base
 
     @countries = %w{Poland Finland Germany}
     @people = Person.find_all
-
-    @elements.date.label = "Pick a date. Only the year will be used."
-    @elements.date.vaadin_events["value-changed"] = "update"
 
     @elements.grid.items = Person.find_all
     @elements.grid.columns = [{"name": "first_name"}, {"name": "last_name"}]
